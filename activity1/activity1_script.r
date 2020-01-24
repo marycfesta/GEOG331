522b05ec496a7c79c9f947a6af3891a40f9cbbb1
Mary Festa '20 <mfesta@colgate.edu>
2020-01-24 12:09:29 -0500

    added script from activity 1 document to r script

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,2 +1,24 @@
 # Activity 1
 print("hello R world")
+
+### FROM INTRO DOCUMENT ###
+# 6 raised to the 6 power
+6^6
+
+# 5 plus 210
+5 + 210
+
+# 3 minus 10
+3 - 10
+
+# name my number
+a <- 2446
+# multiply my number by 5
+a * 5
+
+# make a vector of numbers
+b <- c(2446,40,10000)
+# divide all numbers by 2
+b/2
+
+### QUESTION 3 ###
