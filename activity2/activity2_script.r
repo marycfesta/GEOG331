ab99db5dbe994719bcf6a1e7d274e0a0f1fe9a69
Mary Festa '20 <mfesta@colgate.edu>
2020-01-27 12:05:30 -0500

    added activity2 folder and script

---
+++ activity2/activity2_script.r
@@ -0,0 +1,11 @@
+# make a vector of tree heights in meters
+heights <- c(30,41,20,22)
+# convert to cm
+heights_cm <- heights*100
+heights_cm
+
+#look at the first tree height
+heights[1]
+
+#look at the 2nd and 3rd tree heights
+heights[2:3]
