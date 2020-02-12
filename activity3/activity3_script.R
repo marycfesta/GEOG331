7e1de6815c07614f997d0f158b6a155362cbd000
Mary Festa '20 <mfesta@colgate.edu>
2020-02-12 12:10:40 -0500

    added up to measurements outside of sensor capabilities section to activity 3 script

--- activity3/activity3_script.R
+++ activity3/activity3_script.R
@@ -1,6 +1,7 @@
-### TESTING YOUR CODE ###
+##### TESTING YOUR CODE #####
 
 #create a function. The names of the arguements for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
+#R doesn't have a built-in assert function like Python
 assert <- function(statement,err.message){
   #if evaluates if a statement is true or false for a single item
   if(statement == FALSE){
@@ -20,7 +21,7 @@ a <- c(1,2,3,4)
 b <- c(8,4,5)
 assert(length(a) == length(b), "error: unequal length")
 
-### UNDERSTANDING THE NATURE OF YOUR QA/QC ###
+##### UNDERSTANDING THE NATURE OF YOUR QA/QC #####
 
 #read in the data file
 #skip the first 3 rows since there is additional column info
@@ -43,7 +44,9 @@ colnames(datW) <-   colnames(sensorInfo)
 #preview data
 print(datW[1,])
 
-### DATA QA/QC ###
+##### DATA QA/QC #####
+
+### USING PACKAGES ###
 
 #use install.packages to install lubridate
 #install.packages(c("lubridate"))
@@ -55,3 +58,70 @@ print(datW[1,])
 library(lubridate)
 
 ### WORKING WITH DATES ###
+
+#convert to standardized format
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calcualtions
+datW[1,]
+
+### CHECKING MISSING DATA ###
+
+#see how many values have missing data for each sensor observation
+#air temperature
+length(which(is.na(datW$air.temperature)))
+
+#wind speed
+length(which(is.na(datW$wind.speed)))
+
+#precipitation
+length(which(is.na(datW$precipitation)))
+
+#soil temperature
+length(which(is.na(datW$soil.moisture)))
+
+#soil moisture
+length(which(is.na(datW$soil.temp)))
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+###### SETTING UP TESTS FOR QA/QC ######
+
+### VISUAL CHECKS ###
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+#I'm going to make a new column to work with that indicates that I am conducting QAQC
+#because overwriting values should be done cautiously and can lead to confusing issues.
+#It can be particularily confusing when you are just learning R.
+#Here I'm using the ifelse function
+#the first argument is a logical statement to be evaluated as true or false on a vector
+#the second argument is the value that my air.tempQ1 column will be given if the statement
+#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
+#In this case it is just given the air temperature value
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+
+### REALISTIC VALUES ###
+
+#check the values at the extreme range of the data
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]  
+
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,]  
