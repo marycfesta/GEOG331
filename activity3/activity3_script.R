9fff2cd0b3e97c1a45f23441af5f976496a8eb48
Mary Festa '20 <mfesta@colgate.edu>
2020-02-12 11:57:48 -0500

    added up to QA/QC in activity 3 script

--- activity3/activity3_script.R
+++ activity3/activity3_script.R
@@ -1,3 +1,5 @@
+### TESTING YOUR CODE ###
+
 #create a function. The names of the arguements for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
 assert <- function(statement,err.message){
   #if evaluates if a statement is true or false for a single item
@@ -18,3 +20,38 @@ a <- c(1,2,3,4)
 b <- c(8,4,5)
 assert(length(a) == length(b), "error: unequal length")
 
+### UNDERSTANDING THE NATURE OF YOUR QA/QC ###
+
+#read in the data file
+#skip the first 3 rows since there is additional column info
+#specify the the NA is designated differently
+datW <- read.csv("y:\\Students\\mfesta\\a03\\bewkes_weather.csv",
+                 na.strings=c("#N/A"), skip=3, header=FALSE)
+#preview data
+print(datW[1,])
+
+#get sensor info from file
+# this data table will contain all relevent units
+sensorInfo <-   read.csv("y:\\Students\\hkropp\\a03\\bewkes_weather.csv",
+                         na.strings=c("#N/A"), nrows=2)
+
+print(sensorInfo)
+
+#get column names from sensorInfo table
+# and set weather station colnames  to be the same
+colnames(datW) <-   colnames(sensorInfo)
+#preview data
+print(datW[1,])
+
+### DATA QA/QC ###
+
+#use install.packages to install lubridate
+#install.packages(c("lubridate"))
+#it is helpful to comment this line after you run this line of code on the computer
+#and the package installs. You really don't want to do this over and over again.
+
+#have to use this every time you want to use a package for your code
+#loads a package to the R working environment
+library(lubridate)
+
+### WORKING WITH DATES ###
