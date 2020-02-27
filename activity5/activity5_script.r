4573455bb55945a25536e5fdc27ab14fd69201e2
Mary Festa <maryfesta@marys-mbp-2.colgate.edu>
2020-02-27 01:25:31 -0500

    added rest of tutorial code to activity 5

--- activity5/activity5_script.r
+++ activity5/activity5_script.r
@@ -11,13 +11,22 @@
 library(lubridate)
 
 #read in streamflow data
-datH <- read.csv("y:\\Students\\mfesta\\a05\\stream_flow_data.csv",
+# datH <- read.csv("y:\\Students\\mfesta\\a05\\stream_flow_data.csv",
+#                  na.strings = c("Eqp"))
+# CHANGE BACK BEFORE SUBMIT
+datH <- read.csv("/Users/maryfesta/Documents/Colgate/Academics/Environmental Data Science/GEOG331/activity5/a05/stream_flow_data.csv",
                  na.strings = c("Eqp"))
+
+
 head(datH)         
 
-#read in precipitation data
-#hourly precipitation is in mm
-datP <- read.csv("y:\\Students\\mfesta\\a05\\2049867.csv")                          
+# #read in precipitation data
+# #hourly precipitation is in mm
+# datP <- read.csv("y:\\Students\\mfesta\\a05\\2049867.csv")   
+
+# CHANGE BACK BEFORE SUBMIT
+datP <- read.csv("/Users/maryfesta/Documents/Colgate/Academics/Environmental Data Science/GEOG331/activity5/a05/2049867.csv")   
+
 head(datP)
 
 #only use most reliable measurements
@@ -200,15 +209,17 @@ legend("topright", c("mean","1 standard deviation"), #legend items
 # change the x axis label to show each month instead of doy
 # make the 2017 line a different color
 
+month_nums <- month(datesD)
+
 #bigger margins
 par(mai=c(1,1,1,1))
 #make plot
 plot(aveF$doy,aveF$dailyAve, 
      type="l", 
-     xlab="Year", 
+     xlab="Month", 
      ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
      lwd=2,
-     ylim=c(0,90),
+     ylim=c(0,180),
      xaxs="i", yaxs ="i",#remove gaps from axes
      axes=FALSE)#no axes
 polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
@@ -220,14 +231,14 @@ polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
 discharge17 <- datD$discharge[datD$year==2017]
 # create a variable to hold doy data from 2017
 doy17 <- datD$doy[datD$year==2017]
-
+        
 #plot the 2017 data
-points(doy17, discharge17)
+lines(doy17, discharge17, col="red")
 
 axis(1, seq(0,360, by=40), #tick intervals
      lab=seq(0,360, by=40)) #tick labels
-axis(2, seq(0,80, by=20),
-     seq(0,80, by=20),
+axis(2, seq(0,180, by=20),
+     seq(0,180, by=20),
      las = 2)#show ticks at 90 degree angle
 legend("topright", c("mean","1 standard deviation"), #legend items
        lwd=c(2,NA),#lines
@@ -235,3 +246,56 @@ legend("topright", c("mean","1 standard deviation"), #legend items
        pch=c(NA,15),#symbols
        bty="n")#no legend border
 # end q5
+
+##### MAKING A HYDROGRAPH #####
+
+#### QUESTION 7 ####
+# end q7
+
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 248 & datD$doy < 250 & datD$year == 2011,]
+hydroP <- datP[datP$doy >= 248 & datP$doy < 250 & datP$year == 2011,]
+
+min(hydroD$discharge)
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#celing rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+        polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+                  hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+                c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+                col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+
+##### MAKING BOX PLOTS AND VIOLIN PLOTS #####
+
+library(ggplot2)
+#specify year as a factor
+datD$yearPlot <- as.factor(datD$year)
+#make a boxplot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+        geom_boxplot()
+
+#make a violin plot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+        geom_violin()
